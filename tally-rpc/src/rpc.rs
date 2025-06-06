39f32def25fa32191964cac9c50f9dfb09233e65
Sam Willcocks <sam@wlcx.cc>
2025-06-06 17:55:47 +0100

    rpc wip config

--- tally-rpc/src/rpc.rs
+++ tally-rpc/src/rpc.rs
@@ -1,12 +1,16 @@
-use postcard_rpc::{endpoints, topics, TopicDirection};
+use heapless::Vec;
+use postcard_rpc::{TopicDirection, endpoints, topics};
 use postcard_schema::Schema;
 use serde::{Deserialize, Serialize};
 
 endpoints! {
     list = ENDPOINTS_LIST;
-    | EndpointTy            | RequestTy     | ResponseTy    | Path      |
-    | ----------            | ---------     | ----------    | ----      |
-    | InfoEndpoint          | ()            | InfoResponse  | "info"    |
+    | EndpointTy            | RequestTy     | ResponseTy        | Path         |
+    | ----------            | ---------     | ----------        | ----         |
+    | InfoEndpoint          | ()            | InfoResponse<'a>  | "info"       |
+    | SetConfigEndpoint     | Config        | ()                | "setconf"    |
+    | StartColorTest        | ()            | bool              | "startcolor" |
+    | StopColorTest         | ()            | bool              | "stopcolor"  |
 }
 
 topics! {
@@ -14,6 +18,7 @@ topics! {
     direction = TopicDirection::ToServer;
     | TopicTy                   | MessageTy     | Path              |
     | -------                   | ---------     | ----              |
+    | ColorTestTopic            | ColorTest     | "colortest"       |
 }
 
 topics! {
@@ -28,19 +33,63 @@ pub enum WireErr {}
 
 // Requests
 
+#[derive(Serialize, Deserialize, Schema, Debug)]
+pub enum IfaceConfig {
+    Static { ip: [u8; 4], mask: u8 },
+    DHCP,
+}
+
+impl From<IfaceConfig> for embassy_net::Config {
+    fn from(value: IfaceConfig) -> Self {
+        match value {
+            IfaceConfig::Static { ip, mask } => {
+                embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+                    address: embassy_net::Ipv4Cidr::new(ip.into(), mask),
+                    gateway: None,
+                    dns_servers: Vec::new(),
+                })
+            }
+            IfaceConfig::DHCP => embassy_net::Config::dhcpv4(Default::default()),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Schema, Debug)]
+pub struct Config {
+    //name: &'a str,
+    pub eth: IfaceConfig,
+    pub eth_leds: bool,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            eth: IfaceConfig::DHCP,
+            eth_leds: true,
+        }
+    }
+}
+
 // Responses
 
 #[derive(Serialize, Deserialize, Schema, Debug)]
-pub struct InfoResponse {
+pub struct InfoResponse<'a> {
+    pub name: &'a str,
     pub mac: [u8; 6],
     pub fw_version: (u8, u8, u8),
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct EthConfig {}
+#[derive(Serialize, Deserialize, Schema, Debug)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
 
-#[derive(Serialize, Deserialize)]
-pub struct Config<'a> {
-    name: &'a str,
-    eth: EthConfig,
+// Topics
+#[derive(Serialize, Deserialize, Schema, Debug)]
+pub struct ColorTest {
+    color: Color,
+    angle: u8,
+    width: u8,
 }
